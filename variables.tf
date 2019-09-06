2c3eda589f666116cb3991d035542163d083f5cb
Yuji Azuma <youg.bb@gmail.com>
2019-09-06 15:49:00 +0900

    2nd

--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
+variable "private_key" {}
 variable "region" {}
 
 variable "compartment_ocid" {}
