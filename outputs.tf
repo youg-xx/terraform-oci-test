00bb328963f7ebe90b0ea4b95c6e4a0c52ef5266
Yuji Azuma <youg.bb@gmail.com>
2019-09-11 15:53:01 +0900

    test6

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 // Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
 # Output the public IP of the instance
-output "db_node_public_ip" {
-    value = ["${data.oci_core_vnic.db_node_vnic.public_ip_address}"]
+output "private_key" {
+    # value = ["${data.oci_core_vnic.db_node_vnic.public_ip_address}"]
+    value = "${var.private_key}"
 }
 
